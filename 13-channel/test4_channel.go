68096728789c3a7772c6dacecd4baabe08ff3042
Venns-Git <venns-email@foxmail.com>
2022-07-04 18:02:47 +0800

    channel&select

---
+++ 13-channel/test4_channel.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+/*
+	select 可以监控多个channel
+*/
+func fibonacii(c, quit chan int) {
+	x, y := 1, 1
+	for {
+		select {
+		case c <- x:
+			// 如果c可写,则该case就会进来
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+
+	go func() {
+		for i := 0; i < 6; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+
+	fibonacii(c, quit)
+
+}
